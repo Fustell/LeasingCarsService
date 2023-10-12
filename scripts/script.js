2e4706dcd29006b391dc76647b2f0a35c375d535
Roman <roman24613@gmail.com>
2023-10-12 21:19:10 +0300

    Upload final result

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+document.getElementById("main-action").onclick = function () {
+    document.getElementById("cars").scrollIntoView({ behavior: "smooth" });
+};
+
+let buttons = document.getElementsByClassName("car-button");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].onclick = function () {
+    document.getElementById("price").scrollIntoView({ behavior: "smooth" });
+  };
+}
+
+document.getElementById("price-action").onclick = function () {
+    if (document.getElementById("name").value === "") {
+      alert("Заповніть поле ім'я!");
+    } else if (document.getElementById("phone").value === "") {
+      alert("Заповніть поле телефон!");
+    } else if (document.getElementById("car").value === "") {
+      alert("Заповніть поле автомобіль!");
+    } else {
+      alert("Спасибі за заявку. Ми зв'яжемося з Вами незабаром.");
+    }
+  };
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    let layer = document.querySelector(".price-image");
+    document.addEventListener("mousemove", (event) => {
+      layer.style.transform =
+        "translate3d(" +
+        (event.clientX * 0.5) / "17" +
+        "px," +
+        (event.clientY * 0.8) / "17" +
+        "px,0px)";
+    });
+  
+    const elem = document.querySelector(".main");
+    document.addEventListener("scroll", () => {
+      elem.style.backgroundPositionX =
+        "0" + "2" * window.pageYOffset + "px";
+    });
+  });
+  
